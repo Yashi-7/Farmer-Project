b2c6fcd1e276d71534e2223bbdb9856ae975d482
Yashi-7 <yadavyashi@gmail.com>
2025-09-28 02:00:00 +0530

    Use environment variable for MongoDB connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
+// function connectToDb() {
+//   const MONGO_URI = process.env.DB_CONNECT || "mongodb://127.0.0.1:27017/men-drive";
+
+//   mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("Connected to DB"))
+//     .catch(err => console.error("DB connection error:", err));
+// }
+
 function connectToDb() {
   const MONGO_URI = process.env.DB_CONNECT || "mongodb://127.0.0.1:27017/men-drive";
+  console.log('Connecting to MongoDB URI:', MONGO_URI);  // <--- add this line
 
-  mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(MONGO_URI)
     .then(() => console.log("Connected to DB"))
     .catch(err => console.error("DB connection error:", err));
 }
+
 
 module.exports = connectToDb;
